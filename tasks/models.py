c2ffbcfb6d00dca38b6271ce5d98696de892fc80
Tsoy41k <nikita_tsoy1313@mail.ru>
2026-03-01 02:01:31 +0500

    Initial Django project with Docker and PostgreSQL setup

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ tasks/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+from django import forms
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'completed']
+
+
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create_task.html', {'form': form})
+
+@login_required
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+    task.delete()
+    return redirect('task_list')
+
+
+@login_required
+def update_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'tasks/update_task.html', {'form': form})
