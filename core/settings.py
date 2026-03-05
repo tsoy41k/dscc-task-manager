8c42fe1ea8eda2df6020deccbbddededf38b5a8a
Tsoy41k <nikita_tsoy1313@mail.ru>
2026-03-05 10:53:06 +0500

    Enable debug to see error

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-ci-test-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
